12cf883e1581bb7ec9c1b09c68eca63495f35777
novaterata@gmail.com <novaterata@gmail.com@a1f52008-febd-4ba4-1d53-0dc0f34aaf1c>
2011-11-15 20:53:13 +0000

    blah

---
+++ WebProject3/src/java/beans/ShopingCart.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author novaterata
+ */
+public class ShopingCart implements Serializable{
+	public ShopingCart() {
+	}
+	
+}
